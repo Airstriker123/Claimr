e1a3cde2592dfe4e0d3b490521e96bee5c191f7e
Airstriker123 <114228623+Airstriker123@users.noreply.github.com>
2026-05-25 00:17:15 +1000

    optimize code pwa

--- src/main.tsx
+++ src/main.tsx
@@ -4,33 +4,6 @@ import App from './App.tsx'
 import { AuthProvider } from "@/auth/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
-// @ts-ignore errors
-import { registerSW } from 'virtual:pwa-register'; //import pwa register
-import {Toaster, toast} from "sonner";
-
-function NewUpdate()
-{
-    toast.error("Update new update!");
-    return <Toaster
-        theme="dark"
-        position="bottom-right"
-        toastOptions={{
-            style: {
-                background: 'rgba(0,58,58,0.32)',
-                border: `1px solid ${'#004652'}`,
-                color: '#00ffa4',
-            },
-        }}
-    />
-}
-
-// mount pwa features to application using vite bundler (generate pwa sw, workbox files on compile)
-
-registerSW({
-    immediate: true,
-    onOfflineReady: NewUpdate,
-    onNeedRefresh: NewUpdate,
-});
 
 // Render Client Application to DOM
 createRoot(document.getElementById('root')!).render(
